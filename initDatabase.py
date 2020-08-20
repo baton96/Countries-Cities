a95496f4d89e11d9b46521c808b2fa5fbd84c3be
baton96 <podolce0@gmail.com>
2020-08-20 12:42:59 +0200

    Small general fixes

--- initDatabase.py
+++ initDatabase.py
@@ -1,14 +1,7 @@
 import mysql.connector
 import re
-regex = re.compile('[^a-zA-Z ]')
-files = ('name','animal','color','country','job')
 
-for file in files:
-    with open(file+'.txt',"r") as f:
-        mySet = sorted({regex.sub('', line).lower() for line in f})
-    with open(file+'.txt',"w") as f:
-        for line in mySet:
-            f.write(line+"\n")
+files = ('name','animal','color','country','job')
             
 mydb = mysql.connector.connect(
   host="localhost",
@@ -27,12 +20,9 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 for file in files:
     with open(file+'.txt',"r") as f:
-        mycursor.execute(f"CREATE TABLE {file} (value VARCHAR({len(max(f,key=len).strip())}))")
+        mycursor.execute(f"CREATE TABLE {file} (value VARCHAR({len(max(f, key=len).strip())}))")
     mycursor.execute(f"LOAD DATA LOCAL INFILE '{file}.txt' INTO TABLE {file}")
-    ''' 
-    with open(file+'.txt',"r") as f:
-        mycursor.execute("CREATE TABLE %s (value VARCHAR(%d))"%(file, len(max(f,key=len).strip())))
-    with open(file+'.txt',"r") as f:
-        mycursor.executemany("INSERT INTO %s VALUES "%file + "(%s)", [(line.strip(),) for line in f])
-    ''' 
+    #with open(file+'.txt',"r") as f:
+    #    mycursor.executemany("INSERT INTO %s VALUES "%file + "(%s)", [(line.strip(),) for line in f])
+ 
 mydb.commit()
