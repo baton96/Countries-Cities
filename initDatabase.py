a0f56820a2a1624c796d47c8a76d5cc395889237
baton96 <podolce0@gmail.com>
2020-08-22 14:02:34 +0200

    Code refactor

--- initDatabase.py
+++ initDatabase.py
@@ -1,28 +1,20 @@
-import mysql.connector
-import re
+from mysql.connector import connect
 
-files = ('name','animal','color','country','job')
-            
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd=""
-)
+files = ('name', 'animal', 'color', 'country', 'job')
+conOptions = {'host': 'localhost', 'user': 'root', 'passwd': 'root'}
+
+mydb = connect(**conOptions)
 mycursor = mydb.cursor()
 mycursor.execute('DROP DATABASE IF EXISTS panstwamiasta')
 mycursor.execute('CREATE DATABASE panstwamiasta')
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd="",
-  database="panstwamiasta"
-)
+
+mydb = connect(**conOptions, database='panstwamiasta')
 mycursor = mydb.cursor()
 for file in files:
-    with open(file+'.txt',"r") as f:
-        mycursor.execute(f"CREATE TABLE {file} (value VARCHAR({len(max(f, key=len).strip())}))")
-    mycursor.execute(f"LOAD DATA LOCAL INFILE '{file}.txt' INTO TABLE {file}")
-    #with open(file+'.txt',"r") as f:
-    #    mycursor.executemany("INSERT INTO %s VALUES "%file + "(%s)", [(line.strip(),) for line in f])
- 
+    mycursor.execute(f'CREATE TABLE {file} (value VARCHAR(25))')
+    with open(file + '.txt', 'r') as f:
+        mycursor.executemany(
+            f'INSERT INTO {file} VALUES (%s)', [(line.strip(),) for line in f]
+        )
+
 mydb.commit()
