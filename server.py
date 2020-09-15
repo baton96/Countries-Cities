c8751c374c12b5a75e3341d675857cca55657b2a
baton96 <podolce0@gmail.com>
2020-09-15 12:25:38 +0200

    Code reformat

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ import random
 from mysql.connector import connect
 import websockets
 
-it = 0
 letter = chr(random.randint(65, 90))
 response = {'responses': '', 'letter': letter}
 responses = {}
@@ -18,6 +17,7 @@ mydb = connect(
     database='panstwamiasta'
 )
 mycursor = mydb.cursor()
+fetchone = mycursor.fetchone
 data2 = {'country': '', 'animal': '', 'job': '', 'name': '', 'color': ''}
 
 
@@ -25,41 +25,64 @@ def foo(user):
     global letter
     global response
     mycursor.execute('SELECT ' + ', '.join(
-        [f'(SELECT value FROM {key} WHERE value LIKE "{letter}%" ORDER BY RAND() LIMIT 1)' for key in data2.keys()]))
-    response['example'] = dict(zip(data2.keys(), mycursor.fetchone()))
+        [f'(SELECT value '
+         f'FROM {key} '
+         f'WHERE value LIKE "{letter}%" '
+         f'ORDER BY RAND() '
+         f'LIMIT 1)'
+         for key in data2.keys()]))
+    response['example'] = dict(
+        zip(
+            data2.keys(),
+            fetchone()
+        )
+    )
     return user.send(json.dumps(response))
 
+def changeLetter():
+    global letter
+    letter = chr(random.randint(65, 90))
 
 async def counter(websocket, _):
     global letter
-    global it
     global response
     try:
         async for message in websocket:
             data = json.loads(message)
             if 'login' in data:
                 logins[websocket] = data['login']
-                it += 1
-                if it > 0:
-                    it = 0
-                    await asyncio.wait([foo(user) for user in logins.keys()])
+                if len(logins) > 0: # Enough players, start the game
+                    await asyncio.wait(
+                        map(foo, logins.keys())
+                        # [foo(user) for user in logins.keys()]
+                    )
             else:
                 login = logins[websocket]
                 responses[login] = data
-                mycursor.execute('SELECT ' + ', '.join(
-                    [f'(SELECT value FROM {key} WHERE value="{value}")' for key, value in data.items()]))
-                valid[login] = dict(zip(data.keys(), [i is not None for i in mycursor.fetchone()]))
+                mycursor.execute('SELECT ' + ', '.join([
+                    f'(SELECT value '
+                    f'FROM {key} '
+                    f'WHERE value="{value}")'
+                    for key, value in data.items()
+                ]))
+                valid[login] = dict(
+                    zip(
+                        data.keys(),
+                        map(bool, fetchone())
+                        # [i is not None for i in mycursor.fetchone()]))
+                    )
+                )
 
-                it += 1
-                if it > 0:
-                    it = 0
-                    score = ''
+                if len(responses) > 0: # Enough responses, end this round and start new one
                     letter = chr(random.randint(65, 90))
                     response = {'responses': '', 'letter': letter}
                     for actualKey, actualFields in responses.items():
                         others = [fields for key, fields in responses.items() if key != actualKey]
-                        points = {name: 0 if not valid[actualKey][name] else (
-                            10 if value in [other[name] for other in others] else 15) for name, value in
+
+                        points = {name:
+                                      0 if not valid[actualKey][name] else (
+                                          10 if value in [other[name] for other in others] else 15)
+                                  for name, value in
                                   actualFields.items()}
                         response['responses'] += f'<tr><td>{actualKey}</td><td' + \
                                                  '</td><td'.join(
